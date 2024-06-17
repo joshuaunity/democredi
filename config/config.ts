dd95d21142d338db5018ef61ee07cb9817f7cd86
joshuaunity <oghenerobojosh01@gmail.com>
2024-06-17 01:09:46 +0100

    chore: DB port explicit addition

--- config/config.ts
+++ config/config.ts
@@ -9,6 +9,7 @@ const config = {
         client: 'mysql',
         connection: {
             host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || 3306, 
             user: process.env.DB_USER || 'root',
             password: process.env.DB_PASSWORD || '1234',
             database: process.env.DB_NAME || 'democredi',
